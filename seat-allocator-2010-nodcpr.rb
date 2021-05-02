4d7eec00e22953849ed5b5ff23c8ecbd2d8191ba
flynnwastaken <32378726+flynnwastaken@users.noreply.github.com>
2021-05-01 20:33:07 -0700

    Update seat-allocator-2010-nodcpr.rb

--- seat-allocator-2010-nodcpr.rb
+++ seat-allocator-2010-nodcpr.rb
@@ -1,12 +1,12 @@
-# This hash holds the 2010 census populations of the fifty states. This data
-# is the default.
-statepops = {
+# This hash holds the 2010 census apportionment populations of the fifty states.
+# This data is the default.
+state_pops = {
   "Alabama" => 4_802_982,  "Alaska" => 721_523, "Arizona" => 6_412_700,
   "Arkansas" => 2_926_229, "California" => 37_341_989, "Colorado" => 5_044_930,
   "Connecticut" => 3_581_628, "Delaware" => 900_877,
   "Florida" => 18_900_773, "Georgia" => 9_727_566, "Hawaii" => 1_366_862,
   "Idaho" => 1_573_499, "Illinois" => 12_865_380, "Indiana" => 6_501_582,
-  "Iowa" => 3_053_787, "Kansas" => 2863813, "Kentucky" => 4_350_606,
+  "Iowa" => 3_053_787, "Kansas" => 2_863_813, "Kentucky" => 4_350_606,
   "Louisiana" => 4_553_962, "Maine" => 1_333_074, "Maryland" => 5_789_929,
   "Massachusetts" => 6_559_644, "Michigan" => 9_911_626,
   "Minnesota" => 5_314_879, "Mississippi" => 2_978_240, "Missouri" => 6_011_478,
@@ -19,12 +19,12 @@ statepops = {
   "South Carolina" => 4_645_975, "South Dakota" => 819_761,
   "Tennessee" => 6_375_431, "Texas" => 25_268_418, "Utah" => 2_770_765,
   "Vermont" => 630_337, "Virginia" => 8_037_736, "Washington" => 6_753_369,
-  "West Virginia" => 1_859_815, "Wisconsin" => 5_698_230, "Wyoming" => 568_300
+  "West Virginia" => 1_859_815, "Wisconsin" => 5_698_230, "Wyoming" => 563_626
 }
 
 # According to legislation and the Constitution, all states start with 1 seat,
 # and then the other seats are allocated.
-currentseats = {
+current_seats = {
   "Alabama" => 1,  "Alaska" => 1, "Arizona" => 1, "Arkansas" => 1,
   "California" => 1, "Colorado" => 1, "Connecticut" => 1, "Delaware" => 1,
   "Florida" => 1, "Georgia" => 1, "Hawaii" => 1, "Idaho" => 1, "Illinois" => 1,
@@ -41,7 +41,7 @@ currentseats = {
 }
 
 # This is dummy data that is replaced by the calculations in the code.
-stateprioritynumber = {
+priority_number = {
   "Alabama" => 1,  "Alaska" => 1, "Arizona" => 1, "Arkansas" => 1,
   "California" => 1, "Colorado" => 1, "Connecticut" => 1, "Delaware" => 1,
   "Florida" => 1, "Georgia" => 1, "Hawaii" => 1, "Idaho" => 1, "Illinois" => 1,
@@ -74,28 +74,28 @@ puts %q{
   of Representatives seat allocation, Y/N?
 }
 prompt
-populationquery = $stdin.gets.chomp
+pop_query = $stdin.gets.chomp
 
-  if populationquery.downcase == "y"
-    population = 308_745_538
-  elsif populationquery.downcase == "n"
+  if pop_query.downcase == "y"
+    population = 308_745_538.0
+  elsif pop_query.downcase == "n"
     puts "\nPlease enter the total US population you wish to use."
     prompt
     population = $stdin.gets.chomp.to_f
 
 # Iterates through the list of states, and overwrites the default 2010 census
 # population with user input.
-    statepops.each_key do |name|
+    state_pops.each_key do |name|
       puts "\nPlease enter the population of #{name} you wish to use."
       prompt
-      newstatepop = $stdin.gets.chomp.to_i
-      statepops[name] = newstatepop
-      puts "\nOkay, the population of #{name} is now #{newstatepop}."
+      new_state_pop = $stdin.gets.chomp.to_i
+      state_pops[name] = new_state_pop
+      puts "\nOkay, the population of #{name} is now #{new_state_pop}."
     end
 
 # User must manually confirm their data entry was correct.
     puts "\nCheck if all these numbers are correct:\n\n"
-    puts statepops
+    puts state_pops
     puts "\nAre they correct, Y/N?"
     prompt
     doublecheck = $stdin.gets.chomp
@@ -123,12 +123,18 @@ seatsquery = $stdin.gets.chomp.to_i
       How many seats do you want the House to have?
         Some interesting numbers:
         435 (the current fixed number of seats by law)
-        548 (the "Wyoming Rule")
+        554 (the "Wyoming Rule")
         1_625 (Madisonian)
-        10_292 (Constitutional maximum)
+        10_291 (Constitutional maximum)
     }
     prompt
     seats = $stdin.gets.chomp.to_f.ceil
+
+    if seats < 50
+      die("I'm sorry, that's too few seats.")
+    elsif 10_292 < seats
+      die("I'm sorry, that's too many seats.")
+    end
 
     repratio = (population / seats).round(0)
 
@@ -139,13 +145,19 @@ seatsquery = $stdin.gets.chomp.to_i
     puts %q{
       How many people does the average representative represent?
         Some interesting numbers:
-        709_760 (the current average for 435 seats)
+        718_331 (the current average for 435 seats)
         563_626 (the "Wyoming rule")
         190_000 (Madisonian)
         30_000 (Constitutional minimum)
     }
     prompt
     repratio = $stdin.gets.chomp.to_f
+
+    if repratio < 30_000
+      die("I'm sorry, that's too few people for a district.")
+    elsif 6_174_910 < repratio
+      die("I'm sorry, that's too many people for a district.")
+    end
 
 # Calculating the number of seats.
     seats = (population / repratio).round(0)
@@ -156,33 +168,33 @@ seatsquery = $stdin.gets.chomp.to_i
     die("\nERROR 1: I'm sorry, that answer wasn't valid. Goodbye!\n")
   end
 
-seatstogiveout = seats - 50
-while seatstogiveout > 0
+seats_to_give_out = seats - 50
+while seats_to_give_out > 0
 
 # Takes each state and calculates a priority number for them, then orders them
 # from largest priority number to smallest.
-  statepops.each do |k, v|
-    stateprioritynumber[k] = statepops[k] / Math.sqrt(currentseats[k] * (currentseats[k] + 1))
-    stateprioritynumber = stateprioritynumber.invert.sort { |a, b| b <=> a }.to_h.invert
+  state_pops.each_key do |k|
+    priority_number[k] = state_pops[k] / Math.sqrt(current_seats[k] * (current_seats[k] + 1))
+    priority_number = priority_number.invert.sort { |a, b| b <=> a }.to_h.invert
   end
 
 # Takes the state name from the first position in the sorted priority number
 # hash and assigns one seat to that state.
-  key = stateprioritynumber.keys[0]
-  currentseats[key] += 1
+  key = priority_number.keys[0]
+  current_seats[key] += 1
 
 # This shows each step of the process as individual seats are assigned.
 # Feel free to comment this out if you want less text displayed when you run
 # the program.
-# puts "#{key} now has #{currentseats[key]} seats."
+# puts "#{key} now has #{current_seats[key]} seats."
 
-  seatstogiveout -= 1
+  seats_to_give_out -= 1
 end
 
-puts "\nThe final allocation of seats to states is:\n\n"
+puts "\nThe final allocation of seats to states is:\n"
 
-currentseats.each do |k, v|
+current_seats.each do |k, v|
   puts "#{k}: #{v} seats."
 end
 
-puts "\nThank you for using \"Seat Allocator 2010\".\n"
+puts "\nThank you for using \"Seat Allocator 2010\"."
